fecf4d0d82c66eddaa513eddd2b0fc908f862070
ddasd2001 <ddasd2001@qq.com>
2018-11-09 05:34:49 +0000

    Topic order

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -6,8 +6,8 @@
 
 <div class="row">
     <div class="col-lg-9 col-md-9 topic-list">
-       @if (isset($category))
-       <div class="alert alert-info" role="alert">
+     @if (isset($category))
+     <div class="alert alert-info" role="alert">
         {{ $category->name }} ：{{ $category->description }}
     </div>
     @endif
@@ -15,18 +15,18 @@
 
         <div class="panel-heading">
             <ul class="nav nav-pills">
-                <li role="presentation" class="active"><a href="#">最后回复</a></li>
-                <li role="presentation"><a href="#">最新发布</a></li>
-            </ul>
-        </div>
+               <li class="{{ active_class( ! if_query('order', 'recent') ) }}"><a href="{{ Request::url() }}?order=default">最后回复</a></li>
+               <li class="{{ active_class(if_query('order', 'recent')) }}"><a href="{{ Request::url() }}?order=recent">最新发布</a></li>
+           </ul>
+       </div>
 
-        <div class="panel-body">
-            {{-- 话题列表 --}}
-            @include('topics._topic_list', ['topics' => $topics])
-            {{-- 分页 --}}
-            {!! $topics->appends(Request::except('page'))->render() !!}
-        </div>
+       <div class="panel-body">
+        {{-- 话题列表 --}}
+        @include('topics._topic_list', ['topics' => $topics])
+        {{-- 分页 --}}
+        {!! $topics->appends(Request::except('page'))->render() !!}
     </div>
+</div>
 </div>
 
 <div class="col-lg-3 col-md-3 sidebar">
