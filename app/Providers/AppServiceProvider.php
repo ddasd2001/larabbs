20d69c903500e247057531b0210c96edfe90bcce
ddasd2001 <ddasd2001@qq.com>
2018-11-12 07:32:10 +0000

    User can delete replay

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Horizon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
 
         //
          \Carbon\Carbon::setLocale('zh');
+         Horizon::auth(function ($request) {
+            // return true / false;
+              return true;
+        });
     }
 
     /**
