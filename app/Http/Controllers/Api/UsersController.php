d0c08bcc372e6fa5965025f4dcabe0d76791b796
ddasd2001 <ddasd2001@qq.com>
2018-11-17 07:16:36 +0000

    update image

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Transformers\UserTransformer;
 use App\Http\Requests\Api\UserRequest;
 use Illuminate\Support\Facades\Redis;
+use App\Models\Image;
 
 class UsersController extends Controller
 {
@@ -27,6 +29,33 @@ class UsersController extends Controller
         // 清除验证码缓存
         Redis::del($verification_key);
 
-        return $this->response->created();
+        return $this->response->item($user, new UserTransformer())
+            ->setMeta([
+                'access_token' => \Auth::guard('api')->fromUser($user),
+                'token_type' => 'Bearer',
+                'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+            ])
+            ->setStatusCode(201);
+    }
+
+    public function me()
+    {
+        return $this->response->item($this->user(), new UserTransformer());
+    }
+
+    public function update(UserRequest $request)
+    {
+        $user = $this->user();
+
+        $attributes = $request->only(['name', 'email', 'introduction']);
+
+        if ($request->avatar_image_id) {
+            $image = Image::find($request->avatar_image_id);
+
+            $attributes['avatar'] = $image->path;
+        }
+        $user->update($attributes);
+
+        return $this->response->item($user, new UserTransformer());
     }
 }
