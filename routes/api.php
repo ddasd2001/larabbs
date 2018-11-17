1fec34fc2010681d5e7e0d8c6dbc5d372991b80e
ddasd2001 <ddasd2001@qq.com>
2018-11-17 09:24:11 +0000

    topics show 2

--- routes/api.php
+++ routes/api.php
@@ -51,8 +51,12 @@ $api->version('v1', [
         // 话题列表
         $api->get('topics', 'TopicsController@index')
             ->name('api.topics.index');
+        // 某个用户的话题列表
         $api->get('users/{user}/topics', 'TopicsController@userIndex')
             ->name('api.users.topics.index');
+        // 话题详情
+        $api->get('topics/{topic}', 'TopicsController@show')
+            ->name('api.topics.show');
 
         // 需要 token 验证的接口
         $api->group(['middleware' => 'api.auth'], function($api) {
