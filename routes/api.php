273a142aa8a59ff4ffb23d789cf0f46565daf24d
ddasd2001 <ddasd2001@qq.com>
2018-11-17 09:19:31 +0000

    topic index 2

--- routes/api.php
+++ routes/api.php
@@ -45,8 +45,14 @@ $api->version('v1', [
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
+        // 分类列表
         $api->get('categories', 'CategoriesController@index')
             ->name('api.categories.index');
+        // 话题列表
+        $api->get('topics', 'TopicsController@index')
+            ->name('api.topics.index');
+        $api->get('users/{user}/topics', 'TopicsController@userIndex')
+            ->name('api.users.topics.index');
 
         // 需要 token 验证的接口
         $api->group(['middleware' => 'api.auth'], function($api) {
